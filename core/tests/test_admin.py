3cdd059eb4fc333bde0583ca88b79f6491eb9bc7
iscodand <iscodand@outlook.com>
2023-01-06 07:03:51 -0300

    fixing imports

--- core/tests/test_admin.py
+++ core/tests/test_admin.py
@@ -1,8 +1,10 @@
 """
 Test for the Django admin modifications.
 """
-from django.test import TestCase
-from django.test import Client
+from django.test import (
+    TestCase,
+    Client
+)
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
